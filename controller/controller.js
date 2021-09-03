1f9a8347dd777ab5f687033623e69b458e6a5e1d
Innextinit <localhost@innext.com>
2021-09-03 09:13:24 -0400

    added whatsapp messageing as notification on new data

--- controller/controller.js
+++ controller/controller.js
@@ -1,15 +1,30 @@
 const Scheduler = require("../models/scheduler-model")
 const { customResponse } = require("../validationAndErrors/response")
+const {TWILIO_ACCOUNT_SID} = process.env; 
+const {TWILIO_AUTH_TOKEN} = process.env; 
+const client = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
 class inputValidation {
     static async meetingInput(req, res, next) {
+        let info = {
+            startTime: req.body.startTime,
+            endTime: req.body.endTime,
+            note: req.body.note,
+            title: req.body.title
+        }
+
         try {
-            return new Scheduler({
-                startTime: req.body.startTime,
-                endTime: req.body.endTime,
-                note: req.body.note,
-                title: req.body.title
-            }).save()
+            client.messages.create({ 
+                body:`NEW. T:${info.title}, D:${info.startTime} N:${info.note}`,
+                from: process.env.from,
+                to: process.env.to 
+            })
+            .then(message => console.log(message.sid)) 
+            .done();
+            
+            return new Scheduler(
+                info
+            ).save()
         } catch (error) {
             next(error)
         }
